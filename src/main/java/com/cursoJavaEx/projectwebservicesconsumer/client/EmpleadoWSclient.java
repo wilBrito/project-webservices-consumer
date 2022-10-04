3654c5d19d73b50fde514ef63b43aa66ae5c74e9
Wilson Brito <wil.brito@icloud.com>
2022-10-04 17:15:21 +0200

    Se realiza el commit sobre el consumo de web service por post y get

---
+++ src/main/java/com/cursoJavaEx/projectwebservicesconsumer/client/EmpleadoWSclient.java
@@ -0,0 +1,48 @@
+package com.cursoJavaEx.projectwebservicesconsumer.client;
+
+import java.time.LocalDateTime;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+
+import com.cursoJavaEx.projectwebservicesconsumer.dto.EmpleadoDTO;
+
+
+
+public class EmpleadoWSclient {
+	
+	public static void main(String[] args) {
+		//:::::::::::::::::: GET::::::::::::::::
+		
+//		Client client = ClientBuilder.newClient();
+//		WebTarget webTarget = client.target("http://localhost:8080/project-webservice/cursoJavaEx/empleadosWS").path("consultarEmpleadoRecibiendoInfoResponse").path("3");
+//		
+//		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+//		Response response = invocationBuilder.get();
+//		
+//		EmpleadoDTO employee = response.readEntity(EmpleadoDTO.class);
+//		
+//		System.out.println("Nombre Empleado: " + employee.getNombre());
+		
+		
+		//:::::::::::::::: POST :::::::::::::::::::
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/project-webservice/cursoJavaEx/empleadosWS").path("guardarEmpleado");
+		
+		EmpleadoDTO emple = new EmpleadoDTO("6","Gorda","Pipa", "Motomami",3,LocalDateTime.now());
+		
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.post(Entity.entity(emple, MediaType.APPLICATION_JSON));
+		
+		System.out.println("Nombre Empleado: " + emple.getNombre());
+	}
+
+}
---
+++ src/main/java/com/cursoJavaEx/projectwebservicesconsumer/dto/EmpleadoDTO.java
@@ -0,0 +1,127 @@
+package com.cursoJavaEx.projectwebservicesconsumer.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+
+public class EmpleadoDTO {
+
+	
+	private String numeroEmpleado;
+	
+	private String nombre;
+	
+	private String primerApellido;
+	
+	private String segundoApellido;
+	
+	private int edad;
+	
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	private LocalDateTime fechaCreacion;
+	
+	
+	
+	
+
+	public EmpleadoDTO() {
+		super();
+	}
+
+	public EmpleadoDTO(String numeroEmpleado, String nombre, String primerApellido, String segundoApellido, int edad,
+			LocalDateTime fechaCreacion) {
+		super();
+		this.numeroEmpleado = numeroEmpleado;
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+		this.edad = edad;
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	/**
+	 * @return the numeroEmpleado
+	 */
+	public String getNumeroEmpleado() {
+		return numeroEmpleado;
+	}
+
+	/**
+	 * @param numeroEmpleado the numeroEmpleado to set
+	 */
+	public void setNumeroEmpleado(String numeroEmpleado) {
+		this.numeroEmpleado = numeroEmpleado;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the primerApellido
+	 */
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	/**
+	 * @param primerApellido the primerApellido to set
+	 */
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	/**
+	 * @return the segundoApellido
+	 */
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	/**
+	 * @param segundoApellido the segundoApellido to set
+	 */
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/**
+	 * @return the fechaCreacion
+	 */
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	/**
+	 * @param fechaCreacion the fechaCreacion to set
+	 */
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	
+}
