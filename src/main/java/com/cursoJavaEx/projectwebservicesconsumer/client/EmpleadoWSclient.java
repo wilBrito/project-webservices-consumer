a126e8ccdc2d91540b8088a36f209df7a2a684d1
Wilson Brito <wil.brito@icloud.com>
2022-10-04 17:31:16 +0200

    url de aws

--- src/main/java/com/cursoJavaEx/projectwebservicesconsumer/client/EmpleadoWSclient.java
+++ src/main/java/com/cursoJavaEx/projectwebservicesconsumer/client/EmpleadoWSclient.java
@@ -17,32 +17,35 @@ import com.cursoJavaEx.projectwebservicesconsumer.dto.EmpleadoDTO;
 
 
 public class EmpleadoWSclient {
+	public static  String url = "http://projectwebservices-env.eba-vnett5tt.us-east-1.elasticbeanstalk.com/cursoJavaEx/empleadosWS";
 	
 	public static void main(String[] args) {
+		
+		
 		//:::::::::::::::::: GET::::::::::::::::
 		
-//		Client client = ClientBuilder.newClient();
-//		WebTarget webTarget = client.target("http://localhost:8080/project-webservice/cursoJavaEx/empleadosWS").path("consultarEmpleadoRecibiendoInfoResponse").path("3");
-//		
-//		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-//		Response response = invocationBuilder.get();
-//		
-//		EmpleadoDTO employee = response.readEntity(EmpleadoDTO.class);
-//		
-//		System.out.println("Nombre Empleado: " + employee.getNombre());
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(url).path("consultarEmpleadoRecibiendoInfoResponse").path("3");
+		
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.get();
+		
+		EmpleadoDTO employee = response.readEntity(EmpleadoDTO.class);
+		
+		System.out.println("Nombre Empleado: " + employee.getNombre());
 		
 		
 		//:::::::::::::::: POST :::::::::::::::::::
 		
-		Client client = ClientBuilder.newClient();
-		WebTarget webTarget = client.target("http://localhost:8080/project-webservice/cursoJavaEx/empleadosWS").path("guardarEmpleado");
-		
-		EmpleadoDTO emple = new EmpleadoDTO("6","Gorda","Pipa", "Motomami",3,LocalDateTime.now());
-		
-		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-		Response response = invocationBuilder.post(Entity.entity(emple, MediaType.APPLICATION_JSON));
-		
-		System.out.println("Nombre Empleado: " + emple.getNombre());
+//		Client client = ClientBuilder.newClient();
+//		WebTarget webTarget = client.target("http://localhost:8080/project-webservice/cursoJavaEx/empleadosWS").path("guardarEmpleado");
+//		
+//		EmpleadoDTO emple = new EmpleadoDTO("6","Gorda","Pipa", "Motomami",3,LocalDateTime.now());
+//		
+//		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+//		Response response = invocationBuilder.post(Entity.entity(emple, MediaType.APPLICATION_JSON));
+//		
+//		System.out.println("Nombre Empleado: " + emple.getNombre());
 	}
 
 }
